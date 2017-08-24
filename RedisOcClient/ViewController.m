45ff3b3ea870e1d1ddda72684985a892718f1eb9
wnkpzzz <onlychengzhi@163.com>
2017-08-24 18:31:16 +0800

    细节修改

--- RedisOcClient/ViewController.m
+++ RedisOcClient/ViewController.m
@@ -22,14 +22,14 @@
     
     CocoaRedis *redis = [CocoaRedis new];
     
-    // Connect to server
-    [[[[redis connectWithHost:@"api.ptaxi.cn" port:9806] then:^id(id value) {
-        return [redis auth: @"ptaxi.cn@2" ];
+    [[[[redis connectWithHost:@"请求地址" port: @"端口号"] then:^id(id value) {
+        return [redis auth: @"请求密码" ];
         
     }] then:^id(id value) {
         NSLog(@"Connected");
         NSLog(@"返回结果： %@",value);
         
+        // 发送请求数据：这里的请求数据类型根据后台要求，方法是不一样的！！！
         return [redis rpush:@"ptaxi_track_8888" value:@"[114.000000,22.000000]"];
         
     }] catch:^id(NSError *err) {
